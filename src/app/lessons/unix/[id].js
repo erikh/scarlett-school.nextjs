457d00bb633cc36e36fd2b09b6f812401a23f5bb
Erik Hollensbe <git@hollensbe.org>
2025-03-08 21:06:26 -0800

    still no idea what I'm doing
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/app/lessons/unix/[id].js
+++ src/app/lessons/unix/[id].js
@@ -1,5 +1,5 @@
 import lessons from "./lessons.js";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Unix() {
   const router = useRouter();
--- src/lib/load_lesson/index.js
+++ src/lib/load_lesson/index.js
@@ -1,5 +1,4 @@
-module.exports = function load_lesson(files) {
-  const fs = require("node:fs");
+module.exports = function load_lesson(fs, files) {
   return files.map((file) => ({
     name: file.name,
     path: fs.readFileSync("src/documents/lessons/" + file.path, "utf8"),
