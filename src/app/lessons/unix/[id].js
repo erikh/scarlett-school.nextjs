c3023d2b1d6daf965d8019b59d8ac1ebf064cb12
Erik Hollensbe <git@hollensbe.org>
2025-03-08 20:14:37 -0800

    moar
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/app/lessons/unix/[id].js
+++ src/app/lessons/unix/[id].js
@@ -1,18 +1,43 @@
-import buildLesson from "../build_lesson.js";
 import lessons from "./lessons.js";
 import { useRouter } from "next/router";
 
-export const getStaticPaths = async () => {
-  return {
-    paths: lessons.map((lesson, index) => {
-      params: {
-        name: `/lessons/unix/${index}`;
-      }
-    }),
-    fallback: true,
-  };
-};
-
-export default function Id() {
-  return buildLesson(useRouter().query, "unix", lessons);
+export default function Unix() {
+  const router = useRouter();
+  return (
+    <div className="markdown">
+      <ReactMarkdown
+        remarkPlugins={[[remarkGfm]]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={solarizedlight}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code
+                style={{
+                  paddingLeft: "0.25em",
+                  paddingRight: "0.25em",
+                  border: "1px solid #888",
+                  backgroundColor: "#966",
+                  color: "white",
+                }}
+                className={className}
+                {...props}
+              >
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {lessons[router.query.id]}
+      </ReactMarkdown>
+    </div>
+  );
 }
--- src/app/lessons/unix/page.js
+++
@@ -1,6 +0,0 @@
-import buildLesson from "../build_lesson.js";
-import lessons from "./lessons.js";
-
-export default function Unix() {
-  return buildLesson({}, "unix", lessons);
-}
